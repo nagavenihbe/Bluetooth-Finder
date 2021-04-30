b923146e47599fa7ab6d5e0567207fbb577c7dff
Nagaveni H <nagavenihbe57@gmail.com>
2021-04-30 13:24:08 -0400

    bluetooth basics

---
+++ BluetoothFinder/app/src/main/java/com/example/bluetoothfinder/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bluetoothfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView statusTextView;
+    Button searchButton;
+    public void searchClicked(View view){
+        statusTextView.setText("Searching------->");
+        searchButton.setEnabled(false);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        statusTextView= findViewById(R.id.statusTextView);
+        searchButton= findViewById(R.id.searchButton);
+    }
+}
